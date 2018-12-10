1709d2bcebd1a41517e732e2cfdb31fd569a1425
pawelrutkowski <pawel.rutkowski@htdevelopers.com>
2018-12-10 23:49:49 +0100

    Updates

--- precache-manifest.dc70b218ec6ada9285d29252f2901dee.js
+++ precache-manifest.dc70b218ec6ada9285d29252f2901dee.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "83b43b6b3fa433d37676",
-    "url": "/adventofcode-2018/static/js/main.83b43b6b.chunk.js"
+    "revision": "8ea5212f19a54dcf526d",
+    "url": "/adventofcode-2018/static/js/main.8ea5212f.chunk.js"
   },
   {
     "revision": "496c068e7ccbc2714230",
@@ -12,7 +12,7 @@ self.__precacheManifest = [
     "url": "/adventofcode-2018/static/js/runtime~main.8cb92343.js"
   },
   {
-    "revision": "3daffb6ec1ed5da4a1e9d7d4182686bb",
+    "revision": "6957369d4ce66b7da24cffbbf2270843",
     "url": "/adventofcode-2018/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/adventofcode-2018/precache-manifest.66c63f0a06d382e80298c1e77826274c.js"
+  "/adventofcode-2018/precache-manifest.dc70b218ec6ada9285d29252f2901dee.js"
 );
 
 workbox.clientsClaim();
